b81dd7892e0d5df8cbea8b99aa773ad30cf8d6d6
nmkolp <45236473+nmkolp@users.noreply.github.com>
2019-04-29 19:18:37 +0300

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,10 @@
+﻿import pyperclip
+import secrets
+import sys
+
+n = 16
+if len(sys.argv) > 1:
+    n = int(sys.argv[1])
+password = ''.join(chr(secrets.randbelow(94) + 33) for i in range(n))
+print(password)
+pyperclip.copy(password)
