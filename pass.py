8fdb8046a7c4e274d2d1b1497ab086489c7f6920
nmkolp <45236473+nmkolp@users.noreply.github.com>
2019-09-11 09:21:05 +0300

    Update pass.py

--- pass.py
+++ pass.py
@@ -5,6 +5,6 @@ import sys
 n = 16
 if len(sys.argv) > 1:
     n = int(sys.argv[1])
-password = ''.join(chr(secrets.randbelow(94) + 33) for i in range(n))
+password = ''.join(chr(secrets.randbelow(94) + 33) for _ in range(n))
 print(password)
 pyperclip.copy(password)
